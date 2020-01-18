c498d97b8bb3b9994a87f3d9385e9dd9cb4bbb97
Culoextremo <32410584+Culoextremo@users.noreply.github.com>
2020-01-18 16:44:54 +0100

    Update SelectedScriptableobjectNewInstanceCreator.cs

--- SelectedScriptableobjectNewInstanceCreator.cs
+++ SelectedScriptableobjectNewInstanceCreator.cs
@@ -1,33 +1,31 @@
 using System;
 using System.IO;
 using System.Linq;
-using System.Reflection;
-using UnityEditor;
 using UnityEngine;
+using UnityEditor;
+using UnityEditor.Compilation;
+using Assembly = System.Reflection.Assembly;
+///<summary>
+///Repository link: https://github.com/Culoextremo/Selected-Scriptableobject-New-Instance-Creator
+
+///# Selected-Scriptableobject-New-Instance-Creator
+///Allows creating a new instance of a selected ScriptableObject in Unity without having to create a new asset menu.
+
+///</summary>
+
+///<remarks>
+///# How to use
+///1-Place "SelectedScriptableobjectNewInstanceCreator.cs" file in your project's Editor folder.
+
+///2-Select a file that contains a non-abstract class which is a subclass of ScriptableObject.
+
+///3-On the asset menu, click Create -> Selected Scriptable Object New Instance.
+///</remarks>
 
 namespace Cutremo
 {
-	///<summary>
-	///Repository link: https://github.com/Culoextremo/Selected-Scriptableobject-New-Instance-Creator
-
-	///# Selected-Scriptableobject-New-Instance-Creator
-	///Allows creating a new instance of a selected ScriptableObject in Unity without having to create a new asset menu.
-
-	///</summary>
-
-	///<remarks>
-	///# How to use
-	///1-Place "SelectedScriptableobjectNewInstanceCreator.cs" file in your project's Editor folder.
-
-	///2-Select a file that contains a non-abstract class which is a subclass of ScriptableObject.
-
-	///3-On the asset menu, click Create -> Selected Scriptable Object New Instance.
-	///</remarks>
-
-	public class SelectedScriptableobjectNewInstanceCreator : MonoBehaviour
+	public static class SelectedScriptableobjectNewInstanceCreator 
 	{
-		private static readonly string[] assemblyNames = new string[] { "Assembly-CSharp", "Assembly-CSharp-Editor" };
-
 		[MenuItem("Assets/Create/Selected Scriptableobject New Instance", priority = 1)]
 		private static void CreateAsset()
 		{
@@ -35,7 +33,7 @@ namespace Cutremo
 
 			string fileText = File.ReadAllText(filePath);
 
-			Type fileType = GetScriptType(fileText);
+			Type fileType = GetScriptType(fileText, filePath);
 
 			ScriptableObject instance = ScriptableObject.CreateInstance(fileType);
 
@@ -52,7 +50,7 @@ namespace Cutremo
 			AssetDatabase.SaveAssets();
 			AssetDatabase.Refresh();
 		}
-
+		 
 		[MenuItem("Assets/Create/Selected Scriptableobject New Instance", validate = true)]
 		private static bool IsSelectedObjectAnScriptableObject()
 		{
@@ -62,23 +60,14 @@ namespace Cutremo
 			{
 				return false;
 			}
-			UnityEngine.Debug.Log("== Player Assemblies ==");
-			UnityEditor.Compilation.Assembly[] playerAssemblies =
-				UnityEditor.Compilation.CompilationPipeline.GetAssemblies(UnityEditor.Compilation.AssembliesType.Player);
-
-			foreach (var assembly in playerAssemblies)
-			{
-				UnityEngine.Debug.Log(assembly.name);
-			}
 
 			string fileText = File.ReadAllText(filePath);
 
-			Type fileType = GetScriptType(fileText);
+			Type fileType = GetScriptType(fileText, filePath);
 
 			return fileType != null && fileType.IsSubclassOf(typeof(ScriptableObject)) && !fileType.IsAbstract;
 		}
-
-		private static Type GetScriptType(string fileText)
+		private static Type GetScriptType(string fileText, string path)
 		{
 			var delimiters = new string[] { " " };
 			var words = fileText.Split(delimiters, StringSplitOptions.RemoveEmptyEntries);
@@ -87,20 +76,19 @@ namespace Cutremo
 			{
 				if (words[a] == "class")
 				{
-					Assembly[] assemblies = Array.FindAll(AppDomain.CurrentDomain.GetAssemblies(), assembly => assemblyNames.Contains(assembly.GetName().Name));
+					string fullAssemblyName = CompilationPipeline.GetAssemblyNameFromScriptPath(path);
+					string assemblyName = Path.GetFileNameWithoutExtension(fullAssemblyName);
+
+					Assembly assembly = AppDomain.CurrentDomain.GetAssemblies().SingleOrDefault(x => x.GetName().Name == assemblyName);
 
 					Type returnType = null;
 
-					for (int b = 0; b < assemblies.Length; b++)
-					{
-						returnType = assemblies[b].GetType(words[a + 1]);
+					returnType = assembly.GetType(words[a + 1]);
 
-						if (returnType != null)
-						{
-							break;
-						}
+					if (returnType != null)
+					{
+						return returnType;
 					}
-					return returnType;
 				}
 			}
 			return null;
